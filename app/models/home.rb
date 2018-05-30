d349bcf8b91ead169eb3fb587a0f07f8ee34721f
Fernando Ruiz Guzman <fruiz@Fernandos-MacBook-Air.local>
2018-05-30 10:11:12 -0500

    First part of the challenge done

---
+++ app/models/home.rb
@@ -0,0 +1,45 @@
+class Home
+  attr_reader :instruction_set
+  def initialize()
+
+    @instruction_set = "
+
+                                                                                                                                           
+                                                                                                                                       
+       CCCCCCCCCCCCC                                                      kkkkkkkk                    tttt                            
+    CCC::::::::::::C                                                      k::::::k                 ttt:::t                            
+  CC:::::::::::::::C                                                      k::::::k                 t:::::t                            
+ C:::::CCCCCCCC::::C                                                      k::::::k                 t:::::t                            
+C:::::C       CCCCCC   ooooooooooo   nnnn  nnnnnnnn        eeeeeeeeeeee    k:::::k    kkkkkkkttttttt:::::ttttttt      aaaaaaaaaaaaa   
+C:::::C               oo:::::::::::oo n:::nn::::::::nn    ee::::::::::::ee  k:::::k   k:::::k t:::::::::::::::::t      a::::::::::::a  
+C:::::C              o:::::::::::::::on::::::::::::::nn  e::::::eeeee:::::eek:::::k  k:::::k  t:::::::::::::::::t      aaaaaaaaa:::::a 
+C:::::C              o:::::ooooo:::::onn:::::::::::::::ne::::::e     e:::::ek:::::k k:::::k   tttttt:::::::tttttt               a::::a 
+C:::::C              o::::o     o::::o  n:::::nnnn:::::ne:::::::eeeee::::::ek::::::k:::::k          t:::::t              aaaaaaa:::::a 
+C:::::C              o::::o     o::::o  n::::n    n::::ne:::::::::::::::::e k:::::::::::k           t:::::t            aa::::::::::::a 
+C:::::C              o::::o     o::::o  n::::n    n::::ne::::::eeeeeeeeeee  k:::::::::::k           t:::::t           a::::aaaa::::::a 
+C:::::C       CCCCCCo::::o     o::::o  n::::n    n::::ne:::::::e           k::::::k:::::k          t:::::t    tttttta::::a    a:::::a 
+ C:::::CCCCCCCC::::Co:::::ooooo:::::o  n::::n    n::::ne::::::::e         k::::::k k:::::k         t::::::tttt:::::ta::::a    a:::::a 
+  CC:::::::::::::::Co:::::::::::::::o  n::::n    n::::n e::::::::eeeeeeee k::::::k  k:::::k        tt::::::::::::::ta:::::aaaa::::::a 
+    CCC::::::::::::C oo:::::::::::oo   n::::n    n::::n  ee:::::::::::::e k::::::k   k:::::k         tt:::::::::::tt a::::::::::aa:::a
+       CCCCCCCCCCCCC   ooooooooooo     nnnnnn    nnnnnn    eeeeeeeeeeeeee kkkkkkkk    kkkkkkk          ttttttttttt    aaaaaaaaaa  aaaa                                 
+    
+    Hola! ¿Has escuchado del cifrado César?
+
+    Encontré un mensaje y al principio pensé que estaba cifrado usando el algoritmo
+    más común y simple utilizando el sexto número primo.
+    
+    Pero resulta que no, creo que está utilizando un número diferente para la rotación. Y estoy muy seguro
+    que es un número que se encuentra en el siguiente rango:
+    
+    [sexto número primo - 5, sexto número primo + 5]
+    
+    ¿Me ayudas a descifrarlo? Te prometo que habrá una sorpresa.
+    
+    ufobrwcgc! zog wbghfiqqwcbsg dofo sz gwuiwsbhs dogc  zog cphwsbsg oqqsrwsbrc o giaawh.qcbsyhoas.wc/dfwnsg
+
+    hi qcbhfogsño sg voqsacgtwbhsqv + sz biasfc rs fchoqwcbsg eis ihwzwnoghs dofo rsgqwtfof sz asbgoxs
+    
+    eié zo tisfno hs oqcadoñs!".chomp
+  end
+
+end

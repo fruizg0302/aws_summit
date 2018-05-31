7b998508499ca9966834acd458fbcd91c16e23c0
Fernando Ruiz Guzman <fruizg0302@me.com>
2018-05-30 23:19:36 -0500

    Adding redirection after time out

--- app/models/award.rb
+++ app/models/award.rb
@@ -1,4 +1,5 @@
 class Award < ApplicationRecord
     validates :contact_email, presence: { message: "No se te olvide tu correo" }
     validates :answer_4, presence: { message: "Te falta el nombre de un integrante" }
+    validates :answer_4, inclusion: {in: %w(joselo daniel rene fernando fer rené eduardo lalo rubí rubi ruby jose luis)}
 end
